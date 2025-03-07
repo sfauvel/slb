5eefad4db8972240ab9c1bbe781f41796b2691cf
sfauvel <sfauvel@gmail.com>
2025-03-07 23:13:16 +0100

    Ajustements

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+pushd "${BASH_SOURCE%/*}"
+
 python3 tournois.py
 git add tournois.csv
 git add docs/tournois/tournois.json
 git commit -m "Mise à jour des tournois"
 git push
+
+popd
