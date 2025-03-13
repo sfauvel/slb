846cfa3a1850331ea3400599298127fc13288e4c
sfauvel <sfauvel@gmail.com>
2025-03-13 23:42:47 +0100

    Add local json validation

---
+++ validate_against_schema.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs');
+const Ajv = require("ajv")
+const ajv = new Ajv()
+
+const schema_file = 'schema_tournois.json'
+const data_file = 'docs/tournois/tournois.json'
+
+try {
+    schema = JSON.parse(fs.readFileSync(schema_file, 'utf8'));   
+} catch (err) {
+    console.error(err);
+}
+
+try {
+    json_read = JSON.parse(fs.readFileSync(data_file, 'utf8'));
+} catch (err) {
+    console.error(err);
+}
+
+console.log("Schema: " + schema_file)
+console.log("Data: " + data_file)
+const valid = ajv.validate(schema, json_read)
+if (!valid) {
+    console.log("Error !!!")
+    console.log(ajv.errors)
+} else {
+    console.log("Ok")
+}
