329e987bf94eded4bd81ff722ca9e3915801da3b
sfauvel <sfauvel@gmail.com>
2025-03-20 09:12:20 +0100

    Extract table generation into a separate file

--- docs/tournois/tournoi.js
+++ docs/tournois/tournoi.js
@@ -1,8 +1,61 @@
 
+
 function is_image(url) {
     return url.toLowerCase().match(/\.(jpeg|jpg|gif|png)$/) != null;
 }
 
+function migrate_all(data) {
+    data.tournois.forEach(tournoi => {
+        migrate(tournoi)
+    })
+    return data
+}
+
+function migrate(tournoi) {
+    tournoi.mail = tournoi["Sujet mail"];
+    tournoi.club = tournoi["Club"];
+    tournoi.date = tournoi["Date"];
+    tournoi.particularite = tournoi["Particularit\u00e9"];
+    tournoi.date_limite = tournoi["Date limite"];
+    return tournoi;
+  }
+
+function creer_cellule_texte(document, texte) {
+    const cellule = document.createElement('td');
+    cellule.textContent = texte;
+    return cellule;
+}
+
+function create_tournament_lines(document, tournoi) {
+     //console.log(tournoi.club)
+     const categories = tournoi.categories === undefined ? ['Non spécifiée'] : tournoi.categories;
+     const dateLimite = tournoi.date_limite === undefined ? '' : tournoi.date_limite;
+
+     rows_content = Array.from(categories, (categorie) => {
+        return  [
+           tournoi.club, 
+           tournoi.date, 
+           dateLimite, 
+           categorie.categorie, 
+           categorie.niveau
+        ];
+     });
+
+     return Array.from(rows_content, (content) => {
+        const row = document.createElement('tr');
+        content.forEach(function(text) {
+            row.appendChild(creer_cellule_texte(document, text));
+        });
+        return row; 
+    });
+}
+
+
 if (typeof module !== 'undefined') {
-    module.exports = is_image;
+    module.exports = {
+        is_image,
+        migrate,
+        migrate_all,
+        create_tournament_lines
+    };
 }
--- spec/tournoi.spec.js
+++ spec/tournoi.spec.js
@@ -1,6 +1,13 @@
-const is_image = require('../docs/tournois/tournoi.js');
+const {is_image, migrate, create_tournament_lines} = require('../docs/tournois/tournoi.js');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 describe("Tournoi", function() {  
+  let document;
+  beforeEach(function(){
+    document = new JSDOM(`<!DOCTYPE html><html><body></body></html>`).window.document;
+  });
+
     it("is image", function() {
       expect(is_image("file.jpeg")).toBe(true);
       expect(is_image("file.jpg")).toBe(true);
@@ -14,7 +21,92 @@ describe("Tournoi", function() {
       expect(is_image("file.json")).toBe(false);
       expect(is_image("file.html")).toBe(false);
       expect(is_image("file.pdf")).toBe(false);
+    })
+
+    it ("Migrate legacy tournament", function() {
+      const tournoi_legacy = {
+        "Sujet mail": "Tournoi du SLB",
+        "Club": "SLB",
+        "Date": "17/05/2025",
+        "Particularit\u00e9": "RAS",
+        "Date limite": "09/05/2025",
+        "categories": [
+            {
+                "categorie": "U15M",
+                "niveau": "X"
+            },
+            {
+                "categorie": "U13M",
+                "niveau": "X"
+            },
+          ],
+          "repertoire": "data/2025_05_17_slb",
+          "ressources": [
+              "data/2025_05_17_slb/mail.html"
+          ]
+      };
+
+      const tournoi = migrate(tournoi_legacy);
+      expect(tournoi.mail).toEqual("Tournoi du SLB");
+      expect(tournoi.date).toEqual("17/05/2025");
+      expect(tournoi.date_limite).toEqual("09/05/2025");
+      expect(tournoi.particularite).toEqual("RAS");
+      expect(tournoi.categories).toEqual([
+        {
+          "categorie": "U15M",
+          "niveau": "X"
+        },
+        {
+            "categorie": "U13M",
+            "niveau": "X"
+        }
+      ]);
+      expect(tournoi.repertoire).toEqual("data/2025_05_17_slb");
+      expect(tournoi.ressources).toEqual([
+        "data/2025_05_17_slb/mail.html"
+      ]);
+    })
+
+    it ("Create tournament rows" , function() {
+     
+      const tournoi = {
+        "mail": "Tournoi du SLB",
+        "club": "SLB",
+        "date": "17/05/2025",
+        "particularite": "RAS",
+        "date_limite": "09/05/2025",
+        "categories": [
+            {
+                "categorie": "U15M",
+                "niveau": "X"
+            },
+            {
+                "categorie": "U13M",
+                "niveau": "X"
+            },
+        ],
+        "repertoire": "data/2025_05_17_paulx_cholti_re",
+        "ressources": [
+            "data/2025_05_17_paulx_cholti_re/mail.html"
+        ]
+    };
+    const rows = create_tournament_lines(document, tournoi);
+    expect(rows.length).toBe(2);
+    {
+      const cells = rows[0].getElementsByTagName('td');
+      expect(cells[0].innerHTML).toBe("SLB");
+      expect(cells[1].innerHTML).toBe("17/05/2025");
+      expect(cells[2].innerHTML).toBe("09/05/2025");
+      expect(cells[3].innerHTML).toBe("U15M");
+      expect(cells[4].innerHTML).toBe("X");
     }
-    );
-  }
-);
+    {
+      const cells = rows[1].getElementsByTagName('td');
+      expect(cells[0].innerHTML).toBe("SLB");
+      expect(cells[1].innerHTML).toBe("17/05/2025");
+      expect(cells[2].innerHTML).toBe("09/05/2025");
+      expect(cells[3].innerHTML).toBe("U13M");
+      expect(cells[4].innerHTML).toBe("X");
+    }
+  })
+});
